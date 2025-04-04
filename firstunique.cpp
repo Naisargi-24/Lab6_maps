acea0428a6f8725421a77548ab867e55232e305b
Naisargi <202401411@daiict.ac.in>
2025-04-04 10:46:15 +0530

    Create firstunique.cpp

---
+++ firstunique.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> charcount;
+
+        for (char c : s) {
+            charcount[c]++;
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            if (charcount[s[i]] == 1) {
+                return i;  
+            }
+        }
+
+        return -1; 
+    }
+};
+
